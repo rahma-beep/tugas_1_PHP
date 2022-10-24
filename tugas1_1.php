7b44fe140876108ed9f399240e90db4189b0b7bd
Nur Rahmadani <nagamas.rahma@gmail.com>
2022-10-25 05:34:28 +0800

    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas1_1.php
@@ -0,0 +1,27 @@
+<?php
+
+$harga1 = "Rp. 13.000";
+$harga2 = "Rp. 15.000";
+$harga3 = "Rp. 13.000";
+$harga4 = "Rp. 1.000";
+$harga5 = "Rp. 1.000";
+$harga6 = "Rp. 3.000";
+$harga7 = "Rp. 5.000";
+$harga8 = "Rp. 4.000";
+$harga9 = "Rp. 5.000";
+
+echo "<h1>Daftar Menu</h1> <br>
+<ul>
+<li>Ayam Goreng = $harga1</li>
+<li>Ayam Bakar = $harga2</li>
+<li>Ayam Sayur = $harga3</li>
+<li>Tempe Goreng = $harga4</li>
+<li>Tahu Goreng = $harga5</li>
+<li>Soto = $harga6</li>
+<li>Nasi Putih = $harga7</li>
+<li>Es Teh = $harga8</li>
+<li>Es Tebu = $harga9</li>
+
+</ul>
+";
+ ?>
---
+++ tugas1_2.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style media="screen">
+    table {
+      width: 20%;
+      border: solid;
+    }
+      td, th {
+        border: solid;
+      }
+    </style>
+  </head>
+  <body>
+    <?php
+    $harga1 = "Rp. 1.000";
+    $harga2 = "Rp. 15.000";
+    $harga3 = "Rp. 20.000";
+    $harga4 = "Rp. 5.000";
+    $harga5 = "Rp. 1.000";
+
+  echo "  <h1>DAFTAR MENU</h1>
+    <table>
+      <tr>
+        <td>No</td>
+        <td>Nama Menu</th>
+        <td>Harga</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Cireng</td>
+        <td>$harga1</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Es Duren</td>
+        <td>$harga2</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Es Milo</td>
+        <td>$harga3</td>
+      </tr><tr>
+        <td>4</td>
+        <td>Es Teh</td>
+        <td>$harga4</td>
+      </tr><tr>
+        <td>5</td>
+        <td>Piscok</td>
+        <td>$harga5</td>
+      </tr>
+    </table>";
+
+
+
+     ?>
+
+  </body>
+</html>
